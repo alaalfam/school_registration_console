cef5a1804ab48d1afa14c762304ec5211d642733
Asus <alaalfam@gmail.com>
2022-02-02 21:15:37 +0330

    start project

---
+++ bin/school_registration_console.dart
@@ -0,0 +1,5 @@
+import 'package:school_registration_console/school_registration_console.dart' as school_registration_console;
+
+void main(List<String> arguments) {
+  print('Hello world: ${school_registration_console.calculate()}!');
+}
---
+++ test/school_registration_console_test.dart
@@ -0,0 +1,8 @@
+import 'package:school_registration_console/school_registration_console.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
